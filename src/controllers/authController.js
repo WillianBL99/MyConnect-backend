1a6c801ca29cd76d20c138f7b55e689bd092d6f0
Guilherme Alves <guialvest@gmail.com>
2022-05-11 18:25:32 -0300

    Implementação de sign in/sign up no back

---
+++ src/controllers/authController.js
@@ -0,0 +1,74 @@
+import bcrypt from "bcrypt";
+import dayjs from "dayjs";
+import { v4 as uuid } from "uuid";
+
+import db from "../db.js";
+
+export async function signUp(req, res) {
+  const { name, email, password, image } = req.body;
+  try {
+    const sault = 10;
+    const cryptPassword = bcrypt.hashSync(password, sault);
+    await db.collection("users").insertOne({
+      name: name,
+      email: email,
+      password: cryptPassword,
+      image: image
+    });
+    return res.sendStatus(201); // created
+  } catch (error) {
+    console.log("Error creating new user.");
+    console.log(error);
+    return res.sendStatus(500);
+  }
+}
+
+export async function signIn(req, res) {
+  const { email } = req.body;
+  const user = res.locals.user;
+  try {
+    const token = uuid();
+    sessionControl(email,token);
+    await db.collection("sessions").insertOne({
+      email: email,
+      token: token,
+      time: dayjs().format("DD/MM/YYYY h:mm:ss"),
+    });
+    delete user.password;
+    delete user._id;
+    res.status(200).send({ ...user, token });
+  } catch (error) {
+    console.log("Error recovering user.");
+    console.log(error);
+    return res.sendStatus(500);
+  }
+}
+
+export async function signOut(req, res) {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer", "").trim();
+  if (!token) return res.send(403); // forbidden
+
+  try {
+    await db.collection("sessions").deleteOne({ token });
+    res.sendStatus(200);
+  } catch (error) {
+    console.log("Error logging out.");
+    console.log(error);
+    return res.sendStatus(500);
+  }
+}
+
+async function sessionControl(email,token) {
+  try {
+    const oldSession = await db.collection("sessions").findOne({ email });
+    if (oldSession) {
+      await db.collection("oldSessions").insertOne(oldSession);
+      await db.collection("sessions").deleteOne({ email });
+    }
+  } catch (error) {
+    console.log("Error session definition.");
+    console.log(error);
+    return res.sendStatus(500);
+  }
+}
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,11 +1,14 @@
 import {Router} from "express";
 
 import { signUp, signIn, signOut } from "../controllers/authController.js";
+import {
+    singUpVallidation,signInVallidation
+} from "../middlewares/authMeddleware.js";
 
 const authRouter = Router();
 
-authRouter.post("/signup", signUp);
-authRouter.post("/signin", signIn);
+authRouter.post("/sign-up",singUpVallidation, signUp);
+authRouter.post("/sign-in",signInVallidation, signIn);
 authRouter.get("/signout", signOut);
 
 export default authRouter;
