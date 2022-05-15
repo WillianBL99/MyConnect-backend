802bb6ded7c34b4bea97e67ca34585a07d488037
paulo uilian <uilianlbarros@gmail.com>
2022-05-15 14:18:03 -0300

    fix: env

--- src/db.js
+++ src/db.js
@@ -1,17 +1,17 @@
 import {MongoClient} from "mongodb";
 import dotenv from "dotenv";
+import chalk from "chalk";
 
 dotenv.config();
-console.log(process.env.MONGO_URL);
+
+console.log(chalk.bold.blue('URL:',process.env.MONGO_URL));
 
 let db = null;
-//const mongoClient = new MongoClient(process.env.MONGO_URL);
-const mongoClient = new MongoClient("mongodb://localhost:27017");
+const mongoClient = new MongoClient(process.env.MONGO_URL);
 
 try {
   await mongoClient.connect();
-  //db = mongoClient.db(process.env.DATABASE);
-  db = mongoClient.db("test");
+  db = mongoClient.db(process.env.DATABASE);
   console.log("MongoDB database connected.")
 } catch (error) {
   console.log("Erro connecting to database.");
