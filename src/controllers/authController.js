286c959d9c43dca51dd8a4102a147a5485c7da10
paulo uilian <uilianlbarros@gmail.com>
2022-06-01 00:03:33 -0300

    FIX: checkingSession

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -49,12 +49,14 @@ export async function signIn(req, res) {
 
 export async function signOut(req, res) {
   const { authorization } = req.headers;
-  const token = authorization?.replace("Bearer", "").trim();
-  if (!token) return res.send(403); // forbidden
-
+  
   try {
+    const token = authorization?.replace("Bearer", "").trim();
+    if (!token) return res.send(403); // forbidden
+    
     await db.collection("sessions").deleteOne({ token });
     res.sendStatus(200);
+
   } catch (error) {
     console.log("Error logging out.");
     console.log(error);
